9c0156d20c6d493ed6ab48868f23e76d0607ee5e
haidar <89732397+haidarhamda@users.noreply.github.com>
2024-11-23 12:36:23 +0700

    fix article page

--- src/app/article/[articleSlug]/page.tsx
+++ src/app/article/[articleSlug]/page.tsx
@@ -1,21 +1,10 @@
 "use client"
-import React, { useRef, useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import Image from "next/image";
-// import {useRouter} from "next/router";
-import {useRouter,useSearchParams,useParams} from "next/navigation";
-import {client} from "@/sanity/lib/client";
-import {urlForImage} from "@/sanity/lib/image";
-// type tParams = Promise<{ slug: string }>;
-// const slug = (await params).articleSlug
-// type ArticleData = {
-//     title: string;
-//     slug: string;
-//     author: string;
-//     mainImage: never;
-//     categories: string[];
-//     publishedAt:string;
-//     body:[];
-// };
+import { useParams } from "next/navigation";
+import { client } from "@/sanity/lib/client";
+import { urlForImage } from "@/sanity/lib/image";
+
 interface Article {
     title: string;
     slug: { current: string; _type: 'slug' };
@@ -48,131 +37,51 @@ interface Article {
     }>;
 }
 
-export default function Article({params,}: { params: { articleSlug: string } }) {
+interface TocSection {
+    title: string;
+    ref: string;
+}
 
-    const subtitleRefs = {
-        intro: useRef(null),
-        section1: useRef(null),
-        section2: useRef(null),
-        conclusion: useRef(null),
-    };
-    const {articleSlug}=useParams();
-
-    // const articleData = {
-    //     date: "14 AUGUST 2024",
-    //     title:
-    //         "ITV Highlight Cutting-Edge EV Skills Training at Lincoln College Group and IMI",
-    //     image: "/about-us2.jpg",
-    //     categories: ["Lincoln College", "News", "Higher Education"],
-    //     slug: React.use(params).articleSlug,
-    // }
-
-    // const [articleData, setArticleData] = useState<Article>({
-    //     title: "",
-    //     slug: { current: "", _type: 'slug' },
-    //     author: { _ref: "", _type: 'reference' },
-    //     mainImage: {
-    //         _type: 'image',
-    //         alt: "",
-    //         asset: {
-    //             _ref: "",
-    //             url: "",
-    //         },
-    //     },
-    //     categories: {
-    //         _ref: "",
-    //         _type: 'reference',
-    //         _key: "",
-    //         title: "",
-    //         color: "",
-    //     },
-    //     publishedAt: "",
-    //     body: "",
-    // });
-
-    const [articleData, setArticleData] = useState<Article|null>(null);
+export default function Article() {
+    const { articleSlug } = useParams();
+    const [articleData, setArticleData] = useState<Article | null>(null);
+    const subtitleRefs: { [key: string]: React.RefObject<HTMLHeadingElement> } = {};
+    const [tocSections, setTocSections] = useState<TocSection[]>([]);
 
     useEffect(() => {
-      //   console.log(articleSlug);
-      //   const fetchArticles = async (articleSlug:string|string[]|undefined) => {
-      //       const query = `*[_type == "post" && slug.current == $articleSlug][0]{
-      //   title,
-      //   slug,
-      //   author,
-      //   mainImage,
-      //   categories[]->{
-      //     title,
-      //     color
-      //   },
-      //   publishedAt,
-      //   body
-      // }`;
-      //       console.log(query);
-      //       const param = { articleSlug };
-      //       const fetchedArticles = await client.fetch(query,param);
-      //       console.log(fetchedArticles);
-      //       setArticleData(fetchedArticles);
-      //       console.log(articleData);
-      //   };
-      //   fetchArticles(articleSlug);
         const query = `*[_type == "post" && slug.current == $articleSlug][0]{
             title,
             slug,
             author,
             mainImage,
             categories[]->{
-              title,
-              color
+                title,
+                color
             },
             publishedAt,
             body
-          }`;
+        }`;
         const param = { articleSlug };
-        client.fetch(query,param)
-            .then((data)=>setArticleData(data))
+        client.fetch(query, param)
+            .then((data) => {
+                setArticleData(data);
+            })
             .catch(console.error);
-    }, []);
+    }, [articleSlug]);
 
-    // console.log(articleData);
-
-    const scrollToSection = (ref:React.RefObject<HTMLDivElement>) => {
-        if(ref.current){
+    const scrollToSection = (ref: React.RefObject<HTMLDivElement>) => {
+        if (ref.current) {
             ref.current.scrollIntoView({ behavior: 'smooth' });
         }
     };
 
-    if (!articleData) {
-        return <div>Loading...</div>; // Show loading while data is being fetched
-    } else {
-        console.log(articleData)
-    }
-
-    // const renderBodyContent = () => {
-    //     return articleData.body.map((block) => {
-    //         const { _key, _type, style, children } = block;
-    //
-    //         let Tag = 'p'; // default to <p> if no style
-    //         if (style === 'h2') Tag = 'h2';
-    //         if (style === 'h3') Tag = 'h3';
-    //         if (style === 'normal') Tag = 'p';
-    //
-    //         return (
-    //             <Tag key={_key} className={style === 'h2' ? 'text-3xl font-bold' : style === 'h3' ? 'text-2xl font-semibold' : ''}>
-    //                 {children.map((child) => child.text).join('')}
-    //             </Tag>
-    //         );
-    //     });
-    // };
-
-    const tocSections: { title: string; ref: string }[] = [];
     const renderBodyContent = () => {
-
-        // Iterate over body content to generate dynamic refs and TOC
-        return articleData.body.map((block) => {
-            const { _key, _type, style, children } = block;
+        const toc: { title: string; ref: string }[] = [];
+        return articleData?.body.map((block) => {
+            const { _key, style, children } = block;
             const text = children.map((child) => child.text).join('');
 
-            let Tag = 'p'; // default to <p> if no style
+            let Tag:React.ElementType = 'p'; // default to <p> if no style
             let headerId = ''; // Default header id for normal paragraphs
 
             if (style === 'h2') {
@@ -188,85 +97,59 @@ export default function Article({params,}: { params: { articleSlug: string } }) 
                 if (!subtitleRefs[headerId]) {
                     subtitleRefs[headerId] = React.createRef();
                 }
-                tocSections.push({ title: text, ref: headerId });
+                toc.push({ title: text, ref: headerId });
             }
 
             return (
-                <Tag key={_key} id={headerId} className={style === 'h2' ? 'text-3xl font-bold' : style === 'h3' ? 'text-2xl font-semibold' : ''}>
+                <Tag key={_key} id={headerId} ref={subtitleRefs[headerId]} className={style === 'h2' ? 'text-3xl font-bold my-3' : style === 'h3' ? 'text-2xl font-semibold my-3' : ''}>
                     {text}
                 </Tag>
             );
         });
     };
 
+    useEffect(() => {
+        if (articleData) {
+            const tocSections:TocSection[] = [];
+            articleData.body.forEach((block) => {
+                const { _key, style, children } = block;
+                const text = children.map((child) => child.text).join('');
+
+                if (style === 'h2' || style === 'h3') {
+                    const headerId = `section-${_key}`;
+                    tocSections.push({ title: text, ref: headerId });
+                }
+            });
+            setTocSections(tocSections);
+        }
+    }, [articleData]);
+
+    const formatDate = (date: string) => {
+        const options: Intl.DateTimeFormatOptions = { year: 'numeric', month: 'long', day: 'numeric' };
+        const formattedDate = new Date(date).toLocaleDateString('en-US', options);
+        return formattedDate;
+    };
+
+    if (!articleData) {
+        return <div>Loading...</div>; // Show loading while data is being fetched
+    }
+
     return (
         <div className="p-6 max-w-5xl mx-auto space-y-12 my-20">
             {/* Title Section with Full-Width Image */}
             <section className="w-full mb-12 text-black">
-                <h1 className="text-3xl sm:text-4xl font-bold text-center mt-4">{articleData.title||"aa"}</h1>
+                <h1 className="text-3xl sm:text-4xl font-bold text-center mt-4">{articleData.title}</h1>
                 <div className="text-gray-600 text-sm text-center mt-2">
-                    {/*<span>{articleData.date}</span> | <span>{articleData.categories.join(",")}</span>*/}
-                </div>
+                    <span>{formatDate(articleData.publishedAt)}</span> | <span>{articleData.categories.map((cat) => cat.title).join(", ")}</span>
+                </div>{articleData.categories.join(",")}
                 <Image
-                    src={urlForImage(articleData.mainImage)
-                        .url()}
+                    src={urlForImage(articleData.mainImage).url()}
                     alt={articleData.mainImage.alt || 'Article image'}
                     width={500}  // specify appropriate width
                     height={300} // specify appropriate height
                     className="w-full h-64 object-cover rounded-lg my-4"
                 />
             </section>
-
-            {/*/!* Content and TOC Section *!/*/}
-            {/*<div className="flex flex-col lg:flex-row space-y-8 lg:space-y-0 lg:space-x-8">*/}
-            {/*    /!* Table of Contents *!/*/}
-            {/*    <aside className="w-1/5 lg:sticky lg:top-6 h-max mr-6 ml-3">*/}
-            {/*        <h2 className="text-lg font-semibold mb-4">Table of Contents</h2>*/}
-            {/*        <ul className="space-y-2 text-blue-600">*/}
-            {/*            <li onClick={() => scrollToSection(subtitleRefs.intro)}*/}
-            {/*                className="cursor-pointer hover:underline">*/}
-            {/*                Introduction*/}
-            {/*            </li>*/}
-            {/*            <li onClick={() => scrollToSection(subtitleRefs.section1)}*/}
-            {/*                className="cursor-pointer hover:underline">*/}
-            {/*                Section 1*/}
-            {/*            </li>*/}
-            {/*            <li onClick={() => scrollToSection(subtitleRefs.section2)}*/}
-            {/*                className="cursor-pointer hover:underline">*/}
-            {/*                Section 2*/}
-            {/*            </li>*/}
-            {/*            <li onClick={() => scrollToSection(subtitleRefs.conclusion)}*/}
-            {/*                className="cursor-pointer hover:underline">*/}
-            {/*                Conclusion*/}
-            {/*            </li>*/}
-            {/*        </ul>*/}
-            {/*    </aside>*/}
-            
-            {/*    /!* Blog Content *!/*/}
-            {/*    <article className="w-3/4 text-black">*/}
-            {/*        /!*<section ref={subtitleRefs.intro} className="mb-8">*!/*/}
-            {/*        /!*    <h2 className="text-2xl font-semibold mb-4">Introduction</h2>*!/*/}
-            {/*        /!*    <p>Lorem ipsum odor amet, consectetuer adipiscing elit. Montes platea placerat rutrum class himenaeos nunc accumsan! Cubilia hendrerit lorem nam lacus nam. Purus nam malesuada elit suspendisse rhoncus venenatis tristique molestie. Ad adipiscing ex sapien odio efficitur. Duis dictumst elit luctus egestas, donec vel euismod. Parturient sociosqu sollicitudin non class dictumst commodo. Suspendisse ante mi donec vivamus arcu cras duis.</p>*!/*/}
-            {/*        /!*</section>*!/*/}
-            
-            {/*        /!*<section ref={subtitleRefs.section1} className="mb-8">*!/*/}
-            {/*        /!*    <h2 className="text-2xl font-semibold mb-4">Section 1</h2>*!/*/}
-            {/*        /!*    <p>Lorem ipsum odor amet, consectetuer adipiscing elit. Montes platea placerat rutrum class himenaeos nunc accumsan! Cubilia hendrerit lorem nam lacus nam. Purus nam malesuada elit suspendisse rhoncus venenatis tristique molestie. Ad adipiscing ex sapien odio efficitur. Duis dictumst elit luctus egestas, donec vel euismod. Parturient sociosqu sollicitudin non class dictumst commodo. Suspendisse ante mi donec vivamus arcu cras duis.</p>*!/*/}
-            {/*        /!*</section>*!/*/}
-            
-            {/*        /!*<section ref={subtitleRefs.section2} className="mb-8">*!/*/}
-            {/*        /!*    <h2 className="text-2xl font-semibold mb-4">Section 2</h2>*!/*/}
-            {/*        /!*    <p>Lorem ipsum odor amet, consectetuer adipiscing elit. Montes platea placerat rutrum class himenaeos nunc accumsan! Cubilia hendrerit lorem nam lacus nam. Purus nam malesuada elit suspendisse rhoncus venenatis tristique molestie. Ad adipiscing ex sapien odio efficitur. Duis dictumst elit luctus egestas, donec vel euismod. Parturient sociosqu sollicitudin non class dictumst commodo. Suspendisse ante mi donec vivamus arcu cras duis.</p>*!/*/}
-            {/*        /!*</section>*!/*/}
-            
-            {/*        /!*<section ref={subtitleRefs.conclusion} className="mb-8">*!/*/}
-            {/*        /!*    <h2 className="text-2xl font-semibold mb-4">Conclusion</h2>*!/*/}
-            {/*        /!*    <p>Lorem ipsum odor amet, consectetuer adipiscing elit. Montes platea placerat rutrum class himenaeos nunc accumsan! Cubilia hendrerit lorem nam lacus nam. Purus nam malesuada elit suspendisse rhoncus venenatis tristique molestie. Ad adipiscing ex sapien odio efficitur. Duis dictumst elit luctus egestas, donec vel euismod. Parturient sociosqu sollicitudin non class dictumst commodo. Suspendisse ante mi donec vivamus arcu cras duis.</p>*!/*/}
-            {/*        /!*</section>*!/*/}
-            {/*        <section className="mb-8">*/}
-            {/*            {renderBodyContent()}*/}
-            {/*        </section>*/}
-            {/*    </article>*/}
 
             {/* Content and TOC Section */}
             <div className="flex flex-col lg:flex-row space-y-8 lg:space-y-0 lg:space-x-8 text-black">
@@ -275,7 +158,11 @@ export default function Article({params,}: { params: { articleSlug: string } }) 
                     <h2 className="text-lg font-semibold mb-4">Table of Contents</h2>
                     <ul className="space-y-2 text-blue-600">
                         {tocSections.map((section) => (
-                            <li key={section.ref} onClick={() => scrollToSection(subtitleRefs[section.ref])} className="cursor-pointer hover:underline">
+                            <li
+                                key={section.ref}
+                                onClick={() => scrollToSection(subtitleRefs[section.ref])}
+                                className="cursor-pointer hover:underline"
+                            >
                                 {section.title}
                             </li>
                         ))}
@@ -287,7 +174,6 @@ export default function Article({params,}: { params: { articleSlug: string } }) 
                     <section>{renderBodyContent()}</section>
                 </article>
             </div>
-
         </div>
     );
 }
