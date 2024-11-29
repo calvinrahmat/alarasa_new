84eb33434c211f4ef48e4b5a162a14cca7d23477
root <root@email.com>
2024-11-29 22:11:42 +0700

    fixed hydration error

--- src/components/hero-about.tsx
+++ src/components/hero-about.tsx
@@ -9,8 +9,8 @@ export default function Hero() {
         <Image
           src="/kitchen1.jpg"
           alt="About Hero"
-          layout="fill"
-          objectFit="cover"
+          fill
+          className="object-cover"
           priority
         />
       </div>
@@ -23,11 +23,13 @@ export default function Hero() {
         <h1 className="font-sans md:font-sans text-4xl sm:text-5xl md:text-6xl font-extrabold text-yellow-300 tracking-tight mb-6">
           ALARASA CATERING
         </h1>
-        <p className="text-white text-base md:text-xl lg:text-2xl leading-relaxed font-medium text-justify pe-10">
+        <p className="text-white text-base md:text-xl lg:text-2xl leading-relaxed font-medium text-left pe-10">
           Alarasa Catering merupakan perusahaan catering yang berkomitmen untuk
           menyajikan aneka lauk selera nusantara yang dikemas dalam berbagai
-          macam kemasan untuk memenuhi kebutuhan setiap acara. <br /> <br /> Dengan
-          pengalaman dan dedikasi dalam bidang kuliner, Alarasa Catering
+          macam kemasan untuk memenuhi kebutuhan setiap acara.
+          <br />
+          <br />
+          Dengan pengalaman dan dedikasi dalam bidang kuliner, Alarasa Catering
           menyajikan produk dengan mengutamakan mutu dan kualitas yang didukung
           oleh tenaga kerja professional dan peralatan dapur berstandar komersil
           skala industri.
