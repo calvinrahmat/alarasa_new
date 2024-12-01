c16c10c0fad795767f0064a3be4aae41efa24dff
root <root@email.com>
2024-12-01 20:29:14 +0700

    added tags for sanity

--- sanity.config.ts
+++ sanity.config.ts
@@ -7,6 +7,8 @@
 import {visionTool} from '@sanity/vision'
 import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
+import {tags} from 'sanity-plugin-tags'
+
 
 // Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
 import {apiVersion, dataset, projectId} from './src/sanity/env'
@@ -17,10 +19,12 @@ export default defineConfig({
   basePath: '/studio',
   projectId,
   dataset,
+  
   // Add and edit the content schema in the './sanity/schemaTypes' folder
   schema,
   plugins: [
     structureTool({structure}),
+    tags({}),
     // Vision is for querying with GROQ from inside the Studio
     // https://www.sanity.io/docs/the-vision-plugin
     visionTool({defaultApiVersion: apiVersion}),
