deb8f48df7a59010f9643e287f206ebef63459f6
root <root@email.com>
2025-04-11 16:24:55 +0700

    update link

--- src/components/hero-prasmanan.tsx
+++ src/components/hero-prasmanan.tsx
@@ -62,7 +62,8 @@ export default function Hero() {
           </p>
           <div className="flex flex-col sm:flex-row sm:space-x-4 space-y-4 sm:space-y-0 mb-6">
             <Link
-              https:className="inline-block bg-green-600 text-white font-semibold py-3 px-6 rounded-lg hover:bg-green-800 transition duration-300 w-full text-center" //drive.google.com/file/d/1ydDOx7T02ypTom0s_B1OC_602qUDVrLr/view?usp=sharing
+              href="https://drive.google.com/file/d/1CU2R59XytkyYyaNaJNviTczksAC7XHR9/view?usp=sharing"
+              className="inline-block bg-green-600 text-white font-semibold py-3 px-6 rounded-lg hover:bg-green-800 transition duration-300 w-full text-center"
               target="_blank"
               rel="noopener noreferrer"
             >
