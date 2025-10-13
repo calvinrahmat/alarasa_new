be202395e0048604335362d7ef81de4a81aa5ee7
calvinrahmat <83713045+calvinrahmat@users.noreply.github.com>
2025-10-13 10:37:32 +0700

    Update hero-prasmanan.tsx

--- src/components/hero-prasmanan.tsx
+++ src/components/hero-prasmanan.tsx
@@ -40,7 +40,7 @@ export default function Hero() {
                 <svg className="w-5 h-5 text-green-500" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" />
                 </svg>
-                <p className="text-lg">Bisa Dipesan H-1 sebelum acara</p>
+                <p className="text-lg">Bisa Dipesan H-2 sebelum acara</p>
               </div>
               <div className="flex items-center gap-3">
                 <svg className="w-5 h-5 text-green-500" fill="none" stroke="currentColor" viewBox="0 0 24 24">
